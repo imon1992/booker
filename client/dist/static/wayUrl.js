7bae02584930ba262035c56e6ec78740a2cc2152
student <student@GFL-1457.ad.geeksforless.net>
2017-11-10 17:30:23 +0200

    dfsdsdf

--- client/dist/static/wayUrl.js
+++ client/dist/static/wayUrl.js
@@ -1,24 +1,4 @@
-      // // var authorUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/author/';
-      // var authorUrl= 'http://bookshop1/user14/bookShopRest/client/api/author/';
-      // // var genreUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/genre/';
-      // var genreUrl= 'http://bookshop1/user14/bookShopRest/client/api/genre/';
-      // // var bookUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/book/';
-      // var bookUrl= 'http://bookshop1/user14/bookShopRest/client/api/book/';
-      // // var authUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/auth/';
-      // var authUrl= 'http://bookshop1/user14/bookShopRest/client/api/auth/';
-      // // var userUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/user/';
-      // var userUrl= 'http://bookshop1/user14/bookShopRest/client/api/user/';
-      // // var bagUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/bag/';
-      // var bagUrl= 'http://bookshop1/user14/bookShopRest/client/api/bag/';
-      // // var paymentUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/paymentSystem/';
-      // var paymentUrl= 'http://bookshop1/user14/bookShopRest/client/api/paymentSystem/';
-      // // var orderUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/order/';
-      // var orderUrl= 'http://bookshop1/user14/bookShopRest/client/api/order/';
-      // // var statusUrl= 'http://192.168.0.15/~user14/bookShop1/client/api/orderStatus/';
-      // // var statusUrl= 'http://bookshop1/user14/bookShopRest/client/api/orderStatus/';
-
-      
-      var loginTaken = 'login is already taken';
+     var loginTaken = 'login is already taken';
       var eventUrl= 'http://192.168.0.15/~user14/booker/client/api/event/';
      //var eventUrl= 'http://booker/user14/booker/client/api/event/';
       var authUrl= 'http://192.168.0.15/~user14/booker/client/api/auth/';
